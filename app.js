942e53613fb39a54838ef5470191c08e44b476f8
abdurrehmanjami-arj <abdurrehmanjamiwork@gmail.com>
2025-06-30 19:28:43 +0500

    assignment completed

--- app.js
+++ app.js
@@ -26,3 +26,47 @@
 // console.log(
 //   "Hi, I'm " + fullName + " from " + country + " , and I love " + hobby + "!"
 // );
+
+// Javascript String concatenation Practice:
+
+// Assignment No 1
+// var streetAddres = +prompt("Enter your Street Number:");
+// var city = prompt("Enter your City Name:");
+// var country = prompt("Enter your Country Name:");
+// console.log(
+//   "Your full address is: " +
+//     streetAddres +
+//     " Main St, " +
+//     city +
+//     " , " +
+//     country
+// );
+
+// Assignment No 2
+// var firstName = prompt("Enter your First Name:");
+// var favoriteNumber = +prompt("Enter your Favorite Number:");
+// console.log("Your new username is: " + firstName + favoriteNumber);
+
+// Assignment No 3
+// var firstName = prompt("Enter your First Name:");
+// var lastName = prompt("Enter your Last Name:");
+// console.log(firstName + "." + lastName + "@gmail.com");
+
+// Assignment No 4
+// var favoriteColor = prompt("Enter your Favorite Color Name:");
+// var animal = prompt("Enter Animal Name:");
+// var food = prompt("Enter Food Name:");
+// console.log(
+//   "Your favorite things are: " +
+//     favoriteColor +
+//     " " +
+//     animal +
+//     " and " +
+//     food +
+//     "!"
+// );
+
+// Assignment No 5 using template literal
+// var quote = prompt("Enter your Quote:");
+// var authorName = prompt("Enter Author Name:");
+// console.log(`"${quote}." - ${authorName}`);
