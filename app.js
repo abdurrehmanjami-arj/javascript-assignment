4446eb923850d4f723441f4b362c48a0c1a737b0
abdurrehmanjami-arj <abdurrehmanjamiwork@gmail.com>
2025-06-30 17:23:09 +0500

    first portion completed assignment

---
+++ app.js
@@ -0,0 +1,28 @@
+// Alert, Prompt, Console, and Concatenation
+
+// Assignment No 1
+// var username = prompt("Enter your Name: ");
+// alert("Welcome, " + username + "!");
+
+// Assignment No 2
+// var username = prompt("Enter your Name: ");
+// var age = +prompt("Enter your AGE: ");
+// console.log(username + " is " + age + " years old.");
+
+// Assignment No 3
+// var color = prompt("Enter your favorite color name: ");
+// alert("Wow! " + color + " is a nice color!");
+
+// Assignment No 4
+// var num1 = +prompt("Enter your 1st Number:");
+// var num2 = +prompt("Enter your 2nd Number:");
+// var total = num1 + num2;
+// console.log("The result of " + num1 + " + " + num2 + " is " + total);
+
+// Assignment No 5
+// var fullName = prompt("Enter your Full Name:");
+// var hobby = prompt("Enter your Hobby:");
+// var country = prompt("Enter your Country Name:");
+// console.log(
+//   "Hi, I'm " + fullName + " from " + country + " , and I love " + hobby + "!"
+// );
