be694487b0580d7cc55ec67d9f83ee52357dbc51
abdurrehmanjami-arj <abdurrehmanjamiwork@gmail.com>
2025-11-11 09:23:58 +0500

    completed assignment

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 // Alert, Prompt, Console, and Concatenation
 
 // Assignment No 1
+// solution
 // var username = prompt("Enter your Name: ");
 // alert("Welcome, " + username + "!");
 
 // Assignment No 2
+// solution
 // var username = prompt("Enter your Name: ");
 // var age = +prompt("Enter your AGE: ");
 // console.log(username + " is " + age + " years old.");
 
 // Assignment No 3
+// solution
 // var color = prompt("Enter your favorite color name: ");
 // alert("Wow! " + color + " is a nice color!");
 
 // Assignment No 4
+// solution
 // var num1 = +prompt("Enter your 1st Number:");
 // var num2 = +prompt("Enter your 2nd Number:");
 // var total = num1 + num2;
 // console.log("The result of " + num1 + " + " + num2 + " is " + total);
 
 // Assignment No 5
+// solution
 // var fullName = prompt("Enter your Full Name:");
 // var hobby = prompt("Enter your Hobby:");
 // var country = prompt("Enter your Country Name:");
@@ -27,31 +32,38 @@
 //   "Hi, I'm " + fullName + " from " + country + " , and I love " + hobby + "!"
 // );
 
+// ----------------------------------------------------------------------------------
+
 // Javascript String concatenation Practice:
 
 // Assignment No 1
+// solution
 // var streetAddres = +prompt("Enter your Street Number:");
 // var city = prompt("Enter your City Name:");
 // var country = prompt("Enter your Country Name:");
 // console.log("Your full address is: " + streetAddres + " Main St, " + city + " , " + country);
 
 // Assignment No 2
+// solution
 // var firstName = prompt("Enter your First Name:");
 // var favoriteNumber = +prompt("Enter your Favorite Number:");
 // console.log("Your new username is: " + firstName + favoriteNumber);
 
 // Assignment No 3
+// solution
 // var firstName = prompt("Enter your First Name:");
 // var lastName = prompt("Enter your Last Name:");
 // console.log(firstName + "." + lastName + "@gmail.com");
 
 // Assignment No 4
+// solution
 // var favoriteColor = prompt("Enter your Favorite Color Name:");
 // var animal = prompt("Enter Animal Name:");
 // var food = prompt("Enter Food Name:");
 // console.log("Your favorite things are: " + favoriteColor + " " + animal + " and " + food + "!");
 
 // Assignment No 5 using template literal
+// solution
 // var quote = prompt("Enter your Quote:");
 // var authorName = prompt("Enter Author Name:");
 // console.log(`"${quote}." - ${authorName}`);
